a65added9fdda869ded3cd49dd65b40ddcc0a4a0
Bud Ryerson <bud@budryerson.com>
2021-07-19 14:20:47 -0700

    Delete src directory

--- src/tfmplus/docs/TFMini-S/__init__.py
+++
@@ -1,3 +0,0 @@
-''' These product data sheets are for the TFMini-S,
-    which is very similar to the TFMini-Plus and
-    can use the same 'tfmplus' module.'''
--- src/tfmplus/docs/TI OPT3101/__init__.py
+++
@@ -1,2 +0,0 @@
-''' These data sheets help explain the internal
-    TI chip that is the heart of the device.'''
